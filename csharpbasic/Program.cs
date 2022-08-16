c077ab352ecc52f91937789f75689ed0810c035c
prigesh <me.prigesh@gmail.com>
2022-08-16 19:08:49 +0545

    code to ask address

--- csharpbasic/Program.cs
+++ csharpbasic/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("enter your name");
-var name=Console.ReadLine();
+﻿// Console.WriteLine("enter your name");
+// var name=Console.ReadLine();
 
-Console.WriteLine("hi "+name);
+// Console.WriteLine("hi"+name);
+
+
+Console.WriteLine("enter address=");
+var address =Console.ReadLine();
+address=address.ToUpper();
+Console.WriteLine(address);
