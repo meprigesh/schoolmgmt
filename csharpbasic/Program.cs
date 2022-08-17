8dc4f2acdfe749edfec506c23619fc0461dd1fb5
prigesh <me.prigesh@gmail.com>
2022-08-17 17:02:56 +0545

    program to display odd even numbe

--- csharpbasic/Program.cs
+++ csharpbasic/Program.cs
@@ -1,10 +1,11 @@
-﻿// Console.WriteLine("enter your name");
-// var name=Console.ReadLine();
-
-// Console.WriteLine("hi"+name);
-
-
-Console.WriteLine("enter address=");
-var address =Console.ReadLine();
-address=address.ToUpper();
-Console.WriteLine(address);
+﻿Console.WriteLine("enter a number=");
+var num=Console.ReadLine();
+var number=int.Parse(num);
+if(number%2==0)
+{
+    Console.WriteLine("even number is "+number);
+}
+else
+{
+    Console.WriteLine("odd number="+number);
+}
