3760104af2bce3a92760e5bea4c8e7d2648c50ed
prigesh <me.prigesh@gmail.com>
2022-08-23 07:27:53 +0545

    classwork of odd even and simple intrest

--- csharpbasic/Program.cs
+++ csharpbasic/Program.cs
@@ -1,30 +1,3 @@
-﻿//Code to ask name
-Console.Write("enter name=");
-string name=Console.ReadLine();
-
-//code to ask age
-Console.WriteLine("enter age=");
-var age=Console.ReadLine();
-var Age=byte.Parse(age);
-
-//code to ask weight
-Console.Write("enter weight=");
-short weight=Convert.ToInt16(Console.ReadLine());
-
-//code to dispaly name age and weight
-Console.WriteLine("name="+name);
-Console.WriteLine("age="+Age);
-Console.WriteLine("weight="+weight);
-
-
-/*
-Console.Write("enter gender=");
-char? gender;
-if(gender=='m'||gender=='M')
-{
-    Console.Write(gender);
-}
-else(gender =='f'||gender='F');
-{
-    Console.Write(gender);
-}*/
+﻿ClassWork obj1=new();
+obj1.OddEven();
+obj1.SimpleIntrest();
---
+++ csharpbasic/classwork/classwork.cs
@@ -0,0 +1,23 @@
+class ClassWork
+{
+    internal void OddEven()
+    {
+        int num=25;
+        if(num==0)
+        {
+            Console.WriteLine("number is even");
+        }
+        else
+        {
+            Console.WriteLine("Number is odd");
+        }
+    }
+    internal void SimpleIntrest()
+    {
+        int p=500;
+        int t=5;
+        int r=2;
+        int SI=(p*t*r)/100;
+        Console.WriteLine("Simple Intrest="+SI);
+    }
+}
